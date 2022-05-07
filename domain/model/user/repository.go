1e20fac757dc86489b3c94417775f6cce495277d
Kotaro Abe <abekoh.bcky@gmail.com>
2022-05-07 15:59:45 +0900

    generate moq

--- domain/model/user/repository.go
+++ domain/model/user/repository.go
@@ -1,4 +1,5 @@
 //go:generate mockgen -source=$GOFILE -package=$GOPACKAGE -destination=../../../gomock/domain/model/$GOPACKAGE/$GOFILE
+//go:generate moq -out=../../../moq/domain/model/$GOPACKAGE/$GOFILE -stub . Repository
 package user
 
 import (
